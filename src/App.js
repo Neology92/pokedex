3f0027b999465b594ff4df3b7c48308cd15c730a
Neology92 <oskarlegner@gmail.com>
2020-04-05 00:48:57 +0200

    Setup eslint

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class App extends React.PureComponent {
-	render() {
-		return (
-			<div>
-				<h1>App</h1>
-			</div>
-		);
-	}
+    render() {
+        return (
+            <div>
+                <h1>App</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
